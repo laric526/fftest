c45233446c232bda90fb21e96318b817531cd54c
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-19 15:24:26 -0700

    change script to module

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -1,10 +1,11 @@
 const canvas = document.createElement("canvas");
 
+// W: 240, H: 228
 canvas.setAttribute("width", window.innerWidth);
 canvas.setAttribute("height", window.innerHeight);
 document.body.appendChild(canvas);
 
 const ctx = canvas.getContext("2d");
-s
+
 ctx.fillStyle = "bisque";
-ctx.fillRect(16, 32, 8, 24);
+ctx.fillRect(16, 32, 64, 128);
