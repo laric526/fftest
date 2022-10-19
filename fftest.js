f4dd0884c9de4fafa6eb4ab8e269c1b38fe75de6
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-19 14:47:22 -0700

    add screen info

--- fftest.js
+++ fftest.js
@@ -1,4 +1,16 @@
+const info = document.getElementById("info");
 const keyHistory = document.getElementById("key-history");
+
+addToInfo("Screen Width", screen.width);
+addToInfo("Screen Height", screen.height);
+addToInfo("Available Width", screen.availWidth);
+addToInfo("Available Height", screen.availHeight);
+
+function addToInfo(label, value) {
+    const entry = document.createElement("li");
+    entry.innerHTML = `${label}: value`;
+    info.insertBefore(entry, keyHistory.firstChild);
+}
 
 window.onkeydown = (event) => {
     let key = event.key;
