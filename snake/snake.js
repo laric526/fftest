fabf4ddc677c97d020a113d8371bc786c5107549
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-19 15:13:38 -0700

    set canvas size to window size programmatically

--- snake/snake.js
+++ snake/snake.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+canvas.clientWidth = window.innerWidth;
+canvas.clientHeight = window.innerHeight;
+
 ctx.fillStyle = "bisque";
 ctx.fillRect(16, 32, 8, 24);
