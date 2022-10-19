4865423e21cc48e66e042f4e4f6c060940e36976
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-19 15:17:30 -0700

    create canvas programatically

--- snake/snake.js
+++ snake/snake.js
@@ -1,8 +1,10 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.createElement("canvas");
+
+canvas.setAttribute("width", window.innerWidth);
+canvas.setAttribute("height", window.innerHeight);
+document.body.appendChild(canvas);
+
 const ctx = canvas.getContext("2d");
-
-canvas.clientWidth = window.innerWidth;
-canvas.clientHeight = window.innerHeight;
-
+s
 ctx.fillStyle = "bisque";
 ctx.fillRect(16, 32, 8, 24);
